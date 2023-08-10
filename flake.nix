b504f5548b6821505bd320aacaea7384a6f15c7f
Lucas Liu <lucas.liu@novatti.com>
2023-08-10 16:41:53 +1000

    sort of working flake

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
       forSystem = nixpkgs.lib.genAttrs [
         "x86_64-linux"
         "aarch64-linux"
+        "aarch64-darwin"
       ];
       pkgsFor = forSystem (system:
         import nixpkgs { inherit system; }
@@ -20,14 +21,7 @@
           {
             default = pkgs.stdenv.mkDerivation
               {
-                name = "program";
-                src = self;
-                buildPhase = "clang -o program ./program.c";
-                buildInputs = with pkgs; [
-                  nodejs_20
-                  nodePackages.pnpm
-                ];
-                installPhase = "mkdir -p $out/bin; cp program $out/bin; chmod +x $out/bin/program";
+                buildInputs = [ ];
               };
           }
         );
@@ -38,6 +32,8 @@
           {
             default = pkgs.mkShell {
               buildInputs = with pkgs; self.packages."${system}".default.buildInputs ++ [
+                nodejs
+                nodePackages.pnpm
               ];
             };
           }
